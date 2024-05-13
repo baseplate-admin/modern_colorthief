316dc13e2029ab1c9430c94c6007846b3ae350ba
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-13 21:19:20 +0600

    hnn

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use color_thief::ColorFormat;
-use colors_transform::Rgb;
 use pyo3::prelude::*;
 
 // get_color
@@ -14,27 +13,36 @@ fn get_image_buffer(img: image::DynamicImage) -> (Vec<u8>, ColorFormat) {
         _ => unreachable!(),
     }
 }
+#[pyfunction]
+fn get_color(location: String, quality: Option<u8>) -> PyResult<(u8, u8, u8)> {
+    let img = image::open(&std::path::Path::new(&location)).unwrap();
+    let (buffer, color_type) = get_image_buffer(img);
+
+    let colors = color_thief::get_palette(&buffer, color_type, quality.unwrap_or(10), 10).unwrap();
+
+    Ok((colors[0].r, colors[0].g, colors[0].b))
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn _get_palette(location: String) -> PyResult<Vec<String>> {
+fn get_palette(location: String) -> PyResult<Vec<(u8, u8, u8)>> {
     let img = image::open(&std::path::Path::new(&location)).unwrap();
     let (buffer, color_type) = get_image_buffer(img);
+
     let colors = color_thief::get_palette(&buffer, color_type, 10, 10).unwrap();
 
-    let mut color_vec: Vec<String> = vec![];
+    let color_vec = colors
+        .iter()
+        .map(|color| (color.r, color.g, color.b))
+        .collect();
 
-    for color in colors {
-        let rgb = Rgb::from(color.r as f32, color.g as f32, color.b as f32);
-        let hex = rgb.to_css_hex_string().to_owned();
-        color_vec.push(hex);
-    }
     Ok(color_vec)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn modern_colorthief(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(_get_palette, m)?)?;
+    m.add_function(wrap_pyfunction!(get_palette, m)?)?;
+    m.add_function(wrap_pyfunction!(get_color, m)?)?;
     Ok(())
 }
--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-from modern_colorthief import _get_palette
+from modern_colorthief import get_color
+from colorthief import ColorThief
+
 import os
 
-path = os.path.join(os.getcwd(),'test.jpg')
+path = os.path.join(os.getcwd(), "test.jpg")
 
-x = _get_palette(path)
+x = get_color(path, 10)
+y = ColorThief(path).get_color()
 
 print(x)
+print(y)
