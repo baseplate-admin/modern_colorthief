1739b58b9e23da622cdfeb5432b0297ef93edf5a
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-04-06 22:14:34 +0600

    Refactor image handling in get_palette and get_color (#110)
    Refactor get_palette and get_color functions to use pattern matching for type checking and handling unsupported image types.

--- src/python/modern_colorthief/__init__.py
+++ src/python/modern_colorthief/__init__.py
@@ -9,28 +9,30 @@ __version__ = _modern_colorthief.__version__
 
 def get_palette(
     image: str | bytes | io.BytesIO,
-    color_count: int | None = 10,
-    quality: int | None = 10,
+    color_count: int = 10,
+    quality: int = 10,
 ) -> list[tuple[int, int, int]]:
-    if isinstance(image, str):
-        return _get_palette_given_location(image, color_count, quality)
-
-    if isinstance(image, bytes):
-        return _get_palette_given_bytes(image, color_count, quality)
-
-    if isinstance(image, io.BytesIO):
-        return _get_palette_given_bytes(image.getvalue(), color_count, quality)
+    match image:
+        case str():
+            return _get_palette_given_location(image, color_count, quality)
+        case bytes():
+            return _get_palette_given_bytes(image, color_count, quality)
+        case io.BytesIO():
+            return _get_palette_given_bytes(image.getvalue(), color_count, quality)
+        case _:
+            raise TypeError("Unsupported image type")
 
 
 def get_color(
     image: str | bytes | io.BytesIO,
-    quality: int | None = 10,
+    quality: int = 10,
 ) -> tuple[int, int, int]:
-    if isinstance(image, str):
-        return _get_color_given_location(image, quality)
-
-    if isinstance(image, bytes):
-        return _get_color_given_bytes(image, quality)
-
-    if isinstance(image, io.BytesIO):
-        return _get_color_given_bytes(image.getvalue(), quality)
+    match image:
+        case str():
+            return _get_color_given_location(image, quality)
+        case bytes():
+            return _get_color_given_bytes(image, quality)
+        case io.BytesIO():
+            return _get_color_given_bytes(image.getvalue(), quality)
+        case _:
+            raise TypeError("Unsupported image type")
